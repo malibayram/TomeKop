495ee14f47883a3d5f220ad6c7ba2ee75e540f6c
mehmet ali bayram <malibayram91@gmail.com>
2020-12-17 06:53:05 +0300

    adres

---
+++ TomeKop/Models/Adres.cs
@@ -0,0 +1,9 @@
+public class Adres
+{
+    public int adres_id { get; set; }
+    public int uye_id { get; set; }
+    public string il { get; set; }
+    public string ilce { get; set; }
+    public string mahalle { get; set; }
+    public string adres { get; set; }
+}
--- TomeKop/Utils/SqlCommads.cs
+++ TomeKop/Utils/SqlCommads.cs
@@ -2,26 +2,43 @@ namespace TomeKop.Utils
 {
     public class SqlCommands
     {
+        // Üye işlemleri
         public static readonly string CheckIfUyelerTableExist = "SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'uyeler'";
         public static readonly string CreateUyeTable = @"CREATE TABLE uyeler (
-                                                            UYE_ID SERIAL NOT NULL,
-                                                            DANISMAN_ID INT DEFAULT 0,
-                                                            TC_NO CHAR(11) NOT NULL UNIQUE,
-                                                            ADI VARCHAR(15) NOT NULL, 
-                                                            SOYADI VARCHAR(15) NOT NULL,
-                                                            CINSIYET CHAR,
-                                                            TEL_NO CHAR(10),
-                                                            DOGUM_TARIHI DATE,
-                                                            EMAIL VARCHAR(25) NOT NULL UNIQUE,
-                                                            LOGIN_PASSWORD VARCHAR(20) NOT NULL,
-                                                            PRIMARY KEY (UYE_ID)
+                                                            uye_id SERIAL NOT NULL,
+                                                            danisman_id INT DEFAULT 0,
+                                                            tc_no CHAR(11) NOT NULL UNIQUE,
+                                                            adi VARCHAR(15) NOT NULL, 
+                                                            soyadi VARCHAR(15) NOT NULL,
+                                                            cinsiyet CHAR,
+                                                            tel_no CHAR(10),
+                                                            dogum_tarihi DATE,
+                                                            email VARCHAR(25) NOT NULL UNIQUE,
+                                                            login_password VARCHAR(20) NOT NULL,
+                                                            PRIMARY KEY (uye_id)
                                                         );";
 
-        public static readonly string CreateUserLogin = "INSERT INTO uyeler (DANISMAN_ID, TC_NO, ADI, SOYADI, CINSIYET, TEL_NO, DOGUM_TARIHI, EMAIL, LOGIN_PASSWORD) VALUES (@DANISMAN_ID, @TC_NO, @ADI, @SOYADI, @CINSIYET, @TEL_NO, @DOGUM_TARIHI, @EMAIL, @LOGIN_PASSWORD);";
-        public static readonly string UpdateUserLogin = "UPDATE uyeler SET DANISMAN_ID = @danisman_id, TC_NO = @tc_no, ADI = @adi, SOYADI = @soyadi, CINSIYET = @cinsiyet, TEL_NO = @tel_no, DOGUM_TARIHI = @dogum_tarihi, EMAIL = @email, LOGIN_PASSWORD = @login_password WHERE uye_id = @uye_id;";
+        public static readonly string CreateUserLogin = "INSERT INTO uyeler (danisman_id, tc_no, adi, soyadi, cinsiyet, tel_no, dogum_tarihi, email, login_password) VALUES (@danisman_id, @tc_no, @adi, @soyadi, @cinsiyet, @tel_no, @dogum_tarihi, @email, @login_password);";
+        public static readonly string UpdateUserLogin = "UPDATE uyeler SET danisman_id = @danisman_id, tc_no = @tc_no, adi = @adi, soyadi = @soyadi, cinsiyet = @cinsiyet, tel_no = @tel_no, dogum_tarihi = @dogum_tarihi, email = @email, login_password = @login_password WHERE uye_id = @uye_id;";
         public static readonly string DeleteUser = "DELETE FROM uyeler WHERE uye_id = @uye_id";
-        public static readonly string CheckUserLogin = "SELECT * FROM uyeler WHERE EMAIL = @email AND LOGIN_PASSWORD = @login_password;";
+        public static readonly string CheckUserLogin = "SELECT * FROM uyeler WHERE email = @email AND login_password = @login_password;";
         public static readonly string SelectDanisman = "SELECT adi, soyadi, email, tel_no FROM uyeler WHERE uye_id = @uye_id;";
+        public static readonly string SelectUyeler = "SELECT * FROM uyeler WHERE danisman_id > -1;";
 
+        // Adres İşlemleri
+        public static readonly string CheckIfAdresTableExist = "SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'adresler'";
+        public static readonly string CreateAdresTable = @"CREATE TABLE adresler (
+                                                            adres_id SERIAL NOT NULL PRIMARY KEY,
+                                                            uye_id INT NOT NULL,
+                                                            il VARCHAR(13),
+                                                            ilce VARCHAR(15),
+                                                            mahalle VARCHAR(15),
+                                                            adres VARCHAR(36),
+                                                            FOREIGN KEY (uye_id) REFERENCES uyeler(uye_id) ON DELETE CASCADE
+                                                        );";
+        public static readonly string CreateAdres = "INSERT INTO adresler(uye_id, il, ilce, mahalle, adres) VALUES (@uye_id, @il, @ilce, @mahalle, @adres);";
+        public static readonly string UpdateAdres = "UPDATE adresler SET il = @il, ilce = @ilce, mahalle = @mahalle, adres = @adres WHERE adres_id = @adres_id;";
+        public static readonly string DeleteAdres = "DELETE FROM adresler WHERE adres_id = @adres_id;";
+        public static readonly string SelectAdresler = "SELECT * FROM adresler WHERE uye_id = @uye_id;";
     }
 }
