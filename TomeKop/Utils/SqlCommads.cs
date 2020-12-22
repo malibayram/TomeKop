3e7119493ef78df51506e1d0917fbf421fecf5d3
mehmet ali bayram <malibayram91@gmail.com>
2020-12-22 04:14:53 +0300

    nothing

--- TomeKop/Utils/SqlCommads.cs
+++ TomeKop/Utils/SqlCommads.cs
@@ -5,7 +5,7 @@ namespace TomeKop.Utils
         // Üye işlemleri
         public static readonly string CheckIfUyelerTableExist = "SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'uyeler'";
         public static readonly string CreateUyeTable = @"CREATE TABLE uyeler (
-                                                            uye_id SERIAL NOT NULL,
+                                                            uye_id SERIAL,
                                                             danisman_id INT DEFAULT 0,
                                                             tc_no CHAR(11) NOT NULL UNIQUE,
                                                             adi VARCHAR(15) NOT NULL, 
@@ -28,7 +28,7 @@ namespace TomeKop.Utils
         // Adres İşlemleri
         public static readonly string CheckIfAdresTableExist = "SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'adresler'";
         public static readonly string CreateAdresTable = @"CREATE TABLE adresler (
-                                                            adres_id SERIAL NOT NULL PRIMARY KEY,
+                                                            adres_id SERIAL PRIMARY KEY,
                                                             uye_id INT NOT NULL,
                                                             il VARCHAR(13),
                                                             ilce VARCHAR(15),
@@ -48,6 +48,27 @@ namespace TomeKop.Utils
         public static readonly string CreateDanismanView = "CREATE OR REPLACE VIEW danisman_view AS SELECT adi, soyadi, email, tel_no FROM uyeler WHERE danisman_id = -1;";
         public static readonly string SelectUyeler = "SELECT * FROM uye_view;";
         public static readonly string SelectDanismanlar = "SELECT * FROM danisman_view;";
+
+        // Sequence İşlemleri
+        public static readonly string CreateAraziIdSeq = "CREATE SEQUENCE IF NOT EXISTS arazi_id_seq INCREMENT BY 2 START 2;";
+
+        // Arazi İşlemleri
+        public static readonly string CheckIfAraziTableExist = "SELECT * FROM information_schema.tables WHERE table_schema = 'public' AND table_name = 'araziler'";
+        public static readonly string CreateAraziTable = @"CREATE TABLE araziler (
+                                                            arazi_id nextval('arazi_id_seq') PRIMARY KEY,
+                                                            uye_id INT NOT NULL,
+                                                            parsel 	INT,
+                                                            alan_m2 INT CHECK(alam_m2 > 100 AND alam_m2 < 100000),
+                                                            tapu_sahibi_tc_no CHAR(11) NOT NULL,
+                                                            tapu_tarihi DATE,
+                                                            FOREIGN KEY (uye_id) REFERENCES uyeler(uye_id) ON DELETE CASCADE
+                                                        );";
+        public static readonly string CreateArazi = "INSERT INTO araziler(uye_id, parsel, alan_m2, tapu_sahibi_tc_no, tapu_tarihi) VALUES (@uye_id, @parsel, @alan_m2, @tapu_sahibi_tc_no, @tapu_tarihi);";
+        public static readonly string UpdateArazi = "UPDATE araziler SET parsel = @parsel, alan_m2 = @alan_m2, tapu_sahibi_tc_no = @tapu_sahibi_tc_no, tapu_tarihi = @tapu_tarihi WHERE arazi_id = @arazi_id;";
+        public static readonly string DeleteArazi = "DELETE FROM araziler WHERE arazi_id = @arazi_id;";
+        public static readonly string SelectAraziler = "SELECT * FROM araziler WHERE uye_id = @uye_id;";
+        public static readonly string SelectSumUyeAraziler = "SELECT u.uye_id, SUM(a.alan_m2) FROM araziler a, uyeler u WHERE y.uye_id = u.uye_id GROUP BY u.uye_id HAVING SUM(a.alan_m2) > @alan_m2;";
+
 
     }
 }
