51abce497c64ef9ab3e2b819249e0cbbe36b9b96
mehmet ali bayram <malibayram91@gmail.com>
2020-12-24 15:30:22 +0300

    logs

--- TomeKop/Startup.cs
+++ TomeKop/Startup.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Blazored.LocalStorage;
+using Blazored.SessionStorage;
 using Blazored.Toast;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Components;
@@ -37,7 +37,7 @@ namespace TomeKop
 
             // https://www.nuget.org/profiles/BlazorExtensions
             // https://www.nuget.org/packages/Blazor.ContextMenu/
-            services.AddBlazoredLocalStorage();
+            services.AddBlazoredSessionStorage();
 
             services.AddRazorPages();
             services.AddServerSideBlazor();
